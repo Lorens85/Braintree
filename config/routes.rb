b2bc3d5d983d7a4e3ef05d971210e9d3b749550a
Zamith <zamith.28@gmail.com>
2015-12-05 20:38:30 +0000

    Wraps up authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  get "/sign_up" => "users#new"
-  get "/users" => "users#create"
+  root to: "pages#welcome"
+
+  get "/sign_in" => "sessions#new", as: :sign_in
+  post "/sign_in" => "sessions#create"
+  get "/sign_out" => "sessions#destroy", as: :sign_out
+
+  get "/sign_up" => "users#new", as: :sign_up
+  post "/users" => "users#create"
 end
